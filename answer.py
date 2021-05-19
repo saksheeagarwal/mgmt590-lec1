e6dcc723f32bdefaf708b3d387d1500fad084867
Sakshee Agarwal <saksheeagarwal@Sakshees-MBP.local>
2021-05-19 17:47:29 -0400

    Updated model and comments

--- answer.py
+++ answer.py
@@ -1,12 +1,16 @@
+#Importing libraries
 import pandas as pd
 from transformers.pipelines import pipeline
 
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+#Using Bert pertained model
+hg_comp = pipeline('question-answering', model="bert-large-cased-whole-word-masking-finetuned-squad", tokenizer="bert-large-cased-whole-word-masking-finetuned-squad")
 
+#Reading the csv into a dataframe
 data = pd.read_csv('examples.csv')
 
+#To iterate over each row
 for idx, row in data.iterrows():
     context = row['context']
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
-    print(answer)
+    print("The answer for question ", (idx+1) , " is ", answer)
