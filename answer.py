effe71c95ca9c3faacf0357d28b777d7089f5334
Sakshee Agarwal <saksheeagarwal@Sakshees-MBP.local>
2021-05-20 16:22:49 -0400

    Added model comparison

--- answer.py
+++ answer.py
@@ -2,15 +2,24 @@
 import pandas as pd
 from transformers.pipelines import pipeline
 
-#Using Bert pertained model
+#Using pertained models
 hg_comp = pipeline('question-answering', model="bert-large-cased-whole-word-masking-finetuned-squad", tokenizer="bert-large-cased-whole-word-masking-finetuned-squad")
+hg_comp_1 = pipeline('question-answering', model="bert-large-cased-whole-word-masking-finetuned-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+
 
 #Reading the csv into a dataframe
 data = pd.read_csv('examples.csv')
 
-#To iterate over each row
+print("Answers using Model 1:\n")
 for idx, row in data.iterrows():
     context = row['context']
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
-    print("The answer for question ", (idx+1) , " is ", answer)
+    print("Q", (idx+1), question, ": \n Ans. ", answer)
+
+print("Answers using Model 2:\n")
+for idx, row in data.iterrows():
+    context = row['context']
+    question = row['question']
+    answer = hg_comp_1({'question': question, 'context': context})['answer']
+    print("Q", (idx+1), question, ": \n Ans. ", answer)
