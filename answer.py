ab4ba11dbf0028361ba2b03d7b3553bd14030698
Sakshee Agarwal <saksheeagarwal@Sakshees-MacBook-Pro.local>
2021-05-23 22:59:46 -0400

    Till get and put

--- answer.py
+++ answer.py
@@ -1,25 +1,52 @@
-#Importing libraries
 import pandas as pd
 from transformers.pipelines import pipeline
+from flask import Flask
+from flask import request
+from flask import jsonify
 
-#Using pertained models
-hg_comp = pipeline('question-answering', model="bert-large-cased-whole-word-masking-finetuned-squad", tokenizer="bert-large-cased-whole-word-masking-finetuned-squad")
-hg_comp_1 = pipeline('question-answering', model="bert-large-cased-whole-word-masking-finetuned-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+# Create my flask app
+app = Flask(__name__)
 
 
-#Reading the csv into a dataframe
-data = pd.read_csv('examples.csv')
+@app.route("/models", methods=['GET', 'PUT'])
+def models():
+    out = {
+        "name": data['question'],
+        "tokenizer": data['context'],
+        "model": answer
+    }
 
-print("Answers using Model 1:\n")
-for idx, row in data.iterrows():
-    context = row['context']
-    question = row['question']
-    answer = hg_comp({'question': question, 'context': context})['answer']
-    print("Q", (idx+1), question, ": \n Ans. ", answer)
+    return jsonify(out)
 
-print("Answers using Model 2:\n")
-for idx, row in data.iterrows():
-    context = row['context']
-    question = row['question']
-    answer = hg_comp_1({'question': question, 'context': context})['answer']
-    print("Q", (idx+1), question, ": \n Ans. ", answer)
+
+
+# Define a handler for the /answer path, which
+# processes a JSON payload with a question and
+# context and returns an answer using a Hugging
+# Face model.
+@app.route("/answer", methods=['POST'])
+def answer():
+    # Get the request body data
+    data = request.json
+
+    # Import model
+    hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad",
+                       tokenizer="distilbert-base-uncased-distilled-squad")
+
+    # Answer the answer
+    answer = hg_comp({'question': data['question'], 'context': data['context']})['answer']
+
+    # Create the response body.
+    out = {
+        "question": data['question'],
+        "context": data['context'],
+        "answer": answer
+    }
+
+    return jsonify(out)
+
+
+# Run if running "python answer.py"
+if __name__ == '__main__':
+    # Run our Flask app and start listening for requests!
+    app.run(host='0.0.0.0', port=8000, threaded=True)
