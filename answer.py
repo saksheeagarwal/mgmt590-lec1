3ead553f2d89d206d5bafbf9737d2e12f47ce2d1
saksheeagarwal <saksheeagarwal27@gmail.com>
2021-05-25 20:28:44 -0700

    Assignment2 (#1)

--- answer.py
+++ answer.py
@@ -1,26 +1,152 @@
-#Importing libraries
 import pandas as pd
+from sqlite3_api.example.create_database import sql
 from transformers.pipelines import pipeline
+from flask import Flask
+from flask import request
+from flask import jsonify
+import sqlite3
+import json
+import time
 
-#Using pertained models
-hg_comp = pipeline('question-answering', model="bert-large-cased-whole-word-masking-finetuned-squad", tokenizer="bert-large-cased-whole-word-masking-finetuned-squad")
-hg_comp_1 = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+
+conn = sqlite3.connect('database.db')
+print ("Opened database successfully")
 
 
-#Reading the csv into a dataframe
-data = pd.read_csv('examples.csv')
+conn.execute('CREATE TABLE answered (timestamp DATETIME, model TEXT, answer TEXT, question TEXT, context TEXT)')
+conn.execute('CREATE TABLE models (name TEXT, tokenizer TEXT, model TEXT)')
 
-print("Answers using Model 1:\n")
-for idx, row in data.iterrows():
-    context = row['context']
-    question = row['question']
-    answer = hg_comp({'question': question, 'context': context})['answer']
-    print("Q", (idx+1), question, ": \n Ans. ", answer)
+print ("Table created successfully")
+# Create my flask app
+app = Flask(__name__)
 
-print("Answers using Model 2:\n")
-for idx, row in data.iterrows():
-    context = row['context']
-    question = row['question']
-    answer = hg_comp_1({'question': question, 'context': context})['answer']
-    print("Q", (idx+1), question, ": \n Ans. ", answer)
-    
+@app.route("/models", methods=['DELETE', 'GET', 'PUT'])
+def models():
+    if request.method == 'GET':
+        with sqlite3.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("SELECT * FROM models")
+            rows = cur.fetchall();
+            result = []
+            for row in rows:
+                out = {
+                    "name": row[0],
+                    "tokenizer": row[1],
+                    "model": row[2]
+                }
+                result.append(out)
+        return (jsonify(result))
+
+
+    if request.method == 'PUT':
+        print('in put')
+        datar = json.loads(request.data)
+        name = datar['name']
+        tokenizer = datar['tokenizer']
+        model = datar['model']
+        # print(name,tokenizer,model)
+        with sqlite3.connect("database.db") as con:
+            print("Connected")
+            cur = con.cursor()
+            cur.execute("INSERT INTO models (name,tokenizer,model) VALUES(?, ?, ?)", (name, tokenizer, model))
+            con.commit()
+            cur.execute("SELECT * FROM models")
+            rows = cur.fetchall();
+            result = []
+            for row in rows:
+                out = {
+                    "name": row[0],
+                    "tokenizer": row[1],
+                    "model": row[2]
+                }
+                result.append(out)
+        return (jsonify(result))
+
+    if request.method == 'DELETE':
+        print("in delete")
+        model = request.args.get('model')
+        print(model)
+        with sqlite3.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("DELETE FROM models WHERE name=?", (model,))
+            con.commit()
+
+            #To display output
+            cur.execute("SELECT * FROM models")
+            rows = cur.fetchall()
+            result = []
+            for row in rows:
+                out = {
+                    "name": row[0],
+                    "tokenizer": row[1],
+                    "model": row[2]
+                }
+                result.append(out)
+        return (jsonify(result))
+
+# Define a handler for the /answer path
+
+@app.route("/answer", methods=['POST', 'GET'])
+def answer():
+    if request.method == 'POST':
+        model_name = request.args.get('model')
+        # Get the request body data
+        data = request.json
+        with sqlite3.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("SELECT * FROM models WHERE name=?", (model_name,))
+            row = cur.fetchone()
+
+        # Import model
+            hg_comp = pipeline('question-answering', model=row[2], tokenizer=row[1])
+
+        # Answer the question
+            answer = hg_comp({'question': data['question'], 'context': data['context']})['answer']
+            timestamp = int(time.time())
+            cur.execute("INSERT INTO answered (timestamp, model, answer, question, context ) VALUES(?, ?, ?, ?, ?)",
+                        (timestamp, model_name, answer, data['question'], data['context'] ))
+            print("inserted")
+            con.commit()
+        # Create the response body.
+        out = {
+            "timestamp": timestamp,
+            "model": model_name,
+            "answer": answer,
+            "question": data['question'],
+            "context": data['context']
+
+        }
+
+        return jsonify(out)
+
+    if request.method == 'GET':
+        start = request.args.get('start')
+        end = request.args.get('end')
+        with sqlite3.connect("database.db") as con:
+            cur = con.cursor()
+            query = "SELECT * FROM answered where timestamp > ? and timestamp < ?"
+            params = [start,end]
+            if 'model' in request.args:
+                model_name = request.args.get('model')
+                query += "and model = ?"
+                params += model_name,
+            cur.execute(query,params)
+            rows = cur.fetchall()
+            result = []
+            for row in rows:
+                out = {
+                    "timestamp": row[0],
+                    "answer": row[1],
+                    "model": row[2],
+                    "question":row[3],
+                    "context": row[4]
+                }
+                result.append(out)
+        return (jsonify(result))
+
+# Run if running "python answer.py"
+if __name__ == '__main__':
+    # Run our Flask app and start listening for requests!
+    app.run(host='0.0.0.0', port=8000, threaded=True)
+
+
